b7a5046b42e413ee4fa8e66176704d8db0e34256
liushuai <liu3399shuai@163.com>
2020-06-24 14:56:15 +0800

    fix

--- hotFix热修复/JSPatch-Demo/Demo/iOSDemo/JSPatchDemo/demo.js
+++
@@ -1,8 +0,0 @@
-require('UIColor');
-defineClass('JPViewController', { handleBtn: function(sender)
-            {   var color = UIColor.blueColor();
-                var view = self.view();
-                view.setBackgroundColor(color);
-            },
-        }
-    );
